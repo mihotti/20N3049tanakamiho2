75e3d9ebd0d73a8ce470fabcef6886e4255d11b1
mihotti <120080594+mihotti@users.noreply.github.com>
2023-01-09 22:55:08 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+from model import predict
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+st.sidebar.title("画像認識アプリ")
+st.sidebar.write("オリジナルの画像認識モデルを使って何の画像かを判定します。")
+
+st.sidebar.write("")
+
+img_source = st.sidebar.radio("画像のソースを選択してください。",
+                              ("画像をアップロード", "カメラで撮影"))
+if img_source == "画像をアップロード":
+    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg"])
+elif img_source == "カメラで撮影":
+    img_file = st.camera_input("カメラで撮影")
+
+if img_file is not None:
+    with st.spinner("推定中..."):
+        img = Image.open(img_file)
+        st.image(img, caption="対象の画像", width=480)
+        st.write("")
+
+        # 予測
+        results = predict(img)
+
+        # 結果の表示
+        st.subheader("判定結果")
+        n_top = 1  # 確率が高い順に3位まで返す
+        for result in results[:n_top]:
+            st.write(str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。")
